e6750b45ae2dbe0b79709530889478e524a6b0d2
EricGd <speedyand@gmail.com>
2022-09-26 11:56:17 +0200

    files creation

---
+++ LAB_1/client.erl
@@ -0,0 +1,42 @@
+-module(client).
+%% Exported Functions
+-export([start/2]).
+
+%% API Functions
+start(ServerPid, MyName) ->
+    ClientPid = spawn(fun() -> init_client(ServerPid, MyName) end),
+    process_commands(ServerPid, MyName, ClientPid).
+
+init_client(ServerPid, MyName) ->
+    ServerPid ! {client_join_req, MyName, ClientPid},  %% Enviem missatge amb les nostres dades.
+    process_requests().
+
+%% Local Functions
+%% This is the background task logic
+process_requests() ->
+    receive
+        {join, Name} ->
+            io:format("[JOIN] ~s joined the chat~n", [Name]),
+            process_requests();
+        {leave, Name} ->
+            io:format("[LEFT] ~s left the chat~n", [Name]),
+            process_requests();
+        {message, Name, Text} ->
+            io:format("[~s] ~s", [Name, Text]),
+            process_requests();
+        exit -> 
+            ok
+    end.
+
+%% This is the main task logic
+process_commands(ServerPid, MyName, ClientPid) ->
+    %% Read from standard input and send to server
+    Text = io:get_line("-> "),
+    if 
+        Text  == "exit\n" ->
+            ServerPid ! {client_leave_req, MyName, ClientPid},  %% Enviem la solicitud de marxar
+            ok;
+        true ->
+            ServerPid ! {send, MyName, Text},  %% Enviem el text.
+            process_commands(ServerPid, MyName, ClientPid)
+    end.
---
+++ LAB_1/server.erl
@@ -0,0 +1,31 @@
+-module(server).
+%% Exported Functions
+-export([start/0]).
+
+%% API Functions
+start() ->
+    ServerPid = spawn(fun() -> process_requests([]) end),   % Es crea un nou proces, la seva funció es process_requests.
+    register(myserver, ServerPid).                          % Canviar el pid per un nom simple; "myserver"
+
+process_requests(Clients) ->
+    receive                             % Es suspen el proces esperant per un missatge
+        {client_join_req, Name, From} ->
+            NewClients = [Name|Clients],  %% NewClients es la nova llista amb el nou client
+            broadcast(NewClients, {join, Name}),
+            process_requests(NewClients);  %% La nova llista es NewClients
+        {client_leave_req, Name, From} ->
+            NewClients = lists:delete(Name, Clients),  %% Esborrem "Name" de la llista, i ho guardem a NewClients
+            broadcast(Clients, {exit, Name}),  %% Enviem el missatge de exit a tothom.
+            From ! exit,
+            process_requests(NewClients);  %% La nova llista es NewClients.
+        {send, Name, Text} ->
+            broadcast(Name, Text),  %% Envia el missatge Text al usuari Name.
+            process_requests(Clients);
+        disconnect ->
+            unregister(myserver)
+    end.
+
+%% Local Functions 
+broadcast(PeerList, Message) ->
+    Fun = fun(Peer) -> Peer ! Message end,      % Junta el nom del Peer amb el missatge en una funció
+    lists:map(Fun, PeerList).                   % Reenvia la funció a cada Peer del PeerList.
